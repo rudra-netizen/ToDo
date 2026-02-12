38ae0fc5ff31d419009aa45fa61be33fa86c3463
rudra-netizen <rudrakshtiwari972@gmail.com>
2026-02-12 16:35:36 +0530

    Added Wrapper

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,13 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { ToastContainer } from "react-toastify";
-createRoot(document.getElementById("root")).render(
-  <>
-    <App />
-    <ToastContainer position="top-center" />
-  </>
-);
+import Wrapper from "./Wrapper.jsx";
+createRoot(document.getElementById("root")).render(<Wrapper App={<App />} />);
+{
+  /*
+<>
+  <App />
+  <ToastContainer position="top-center" />
+</>;
+*/
+}
